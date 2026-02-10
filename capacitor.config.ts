64b1e5039664f5705db86a6e8689c9067e397958
Camila Congo <ccongo@estud.tesa.edu.ec>
2026-02-09 20:24:39 -0500

    Add files via upload

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'actividad.v1.descargas',
+  appName: 'actividad-1-descargas',
+  webDir: 'dist'
+};
+
+export default config;
